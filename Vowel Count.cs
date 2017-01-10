2d09b517e6ecb33fdb668ef6a70473ffc7c46010
RezikoChkadua <reziko1010@gmail.com>
2017-01-09 23:18:59 -0800

    volverCount
    Return the number (count) of vowels in the given string We will consider
    a, e, i, o, and u as vowels for this Kata.

---
+++ Vowel Count.cs
@@ -0,0 +1,27 @@
+﻿/* 
+
+        Return the number (count) of vowels in the given string.
+        We will consider a, e, i, o, and u as vowels for this Kata.
+
+ */
+
+using System;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0, i;
+
+        for (i = 0; i < str.Length; i++)
+        {
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+
+    }
+}
